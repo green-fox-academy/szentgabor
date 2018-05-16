46c29c56d17875b0e9ccfce16757d4c29ada091c
Szent Gabor <szent.gabor@gmail.com>
2018-05-16 15:11:18 +0200

    2nd week, Wednesday, 1st round

---
+++ csharp/2nd week/Wednesday/Linecount.cs
@@ -0,0 +1,3 @@
+﻿1>------ Build started: Project: ConsoleApp50, Configuration: Debug Any CPU ------
+1>  ConsoleApp50 -> C:\Users\Gabor\source\repos\ConsoleApp50\ConsoleApp50\bin\Debug\ConsoleApp50.exe
+========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
