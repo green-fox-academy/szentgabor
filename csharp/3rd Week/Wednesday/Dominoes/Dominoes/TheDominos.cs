3c1b03daff8eded3617228e57d13035dcbc1a13a
Szent Gabor <szent.gabor@gmail.com>
2018-05-23 17:43:09 +0200

    Dominos

---
+++ csharp/3rd Week/Wednesday/Dominoes/Dominoes/TheDominos.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Dominoes
+{
+    public class Domino
+    {
+        private readonly int[] Values;
+
+        public Domino(int valueA, int valueB)
+        {
+            this.Values = new int[] { valueA, valueB };
+        }
+
+        public int[] GetValues()
+        {
+            return Values;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("[ {0}, {1} ] ", Values[0], Values[1]);
+        }
+    }
+}
