7ef354f7c276e59deb6008ca46fecf78bebab22c
Szent Gabor <szent.gabor@gmail.com>
2018-05-24 15:35:05 +0200

    Bunny

---
+++ csharp/3rd Week/Thursday/Bunny/Bunny/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bunny
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // We have a number of bunnies and each bunny has two big floppy ears.
+            // We want to compute the total number of ears across all the bunnies 
+            // recursively (without loops or multiplication).
+            int bunnyNumber = 1;
+            Console.WriteLine(BunnyCounter(bunnyNumber));
+            Console.ReadLine();
+        }
+        public static int BunnyCounter(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return BunnyCounter(n - 1) + 2;
+            }
+
+        }
+    }
+}
