fdf7c4d0bf24c3fba8a2d887ef3769305df3ecd1
Szent Gabor <szent.gabor@gmail.com>
2018-05-24 14:48:04 +0200

    Power

---
+++ csharp/3rd Week/Thursday/Power/Power/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Power
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Base = 4;
+            int Power = 3;
+            PowerPlay(Base,Power);
+            Console.WriteLine(PowerPlay(Base,Power));
+            Console.ReadLine();
+
+            // Given base and n that are both 1 or more, compute recursively (no loops)
+            // the value of base to the n power, so powerN(3, 2) is 9 (3 squared).
+        }
+        public static int PowerPlay(int n, int k)
+        {
+            if (n == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return k * PowerPlay(n-1, k);
+            }
+        }
+    }
+}
