a4694f18f7d2f158a33ac64f6546d9589456661c
Szent Gabor <szent.gabor@gmail.com>
2018-05-24 17:33:04 +0200

    String1

---
+++ csharp/3rd Week/Thursday/Strings/Strings/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine(StringChanger("xoxo"));
+            Console.ReadLine();
+        
+            // Given a string, compute recursively (no loops) a new string where all the
+            // lowercase 'x' chars have been changed to 'y' chars.   
+        }
+        public static string StringChanger(string Chain)
+        {
+            if (Chain.Length == 0)
+            {
+                return "";
+            }
+            else
+            {
+            return Chain.Replace("x", "y") + StringChanger(Chain.Remove(0));
+            }
+        }
+    }
+}
