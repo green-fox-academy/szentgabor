0cb673150d1ffc7a8939f2bb584e9d9dcf7d48b1
Szent Gabor <szent.gabor@gmail.com>
2018-05-24 16:33:36 +0200

    Bunny2

---
+++ csharp/3rd Week/Thursday/BunniesAgain/BunniesAgain/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BunniesAgain
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine(BunnyEarCounter(10));
+                Console.ReadLine();
+            }
+            public static int BunnyEarCounter(int n)
+            {
+                if (n == 0)
+                {
+                    return 0;
+                }
+                else if (n % 2 == 1)
+                {
+                    return BunnyEarCounter(n - 1) + 2;
+                }
+                else
+                {
+                    return BunnyEarCounter(n - 1) + 3;
+                }
+            }
+        }
+    }
